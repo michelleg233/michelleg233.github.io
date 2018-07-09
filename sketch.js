12b00dd3928dd93dbf8d6cd64f34a1df23ef8358
michelle <michelle.gordon@live.ocm>
2018-07-09 15:57:07 +0200

    new thing

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
 function setup() {
 createCanvas(600, 400);
-background('green');
+background(0, 0, 50);
 }
 
 function draw() {
 noStroke();
+  fill(0, 0, 100);
   ellipse (mouseX, mouseY, 10, 10);
 
-  fill("orange")
+  fill (50, 0, 0);
+  triangle (300, 100, 350, 300, 400, 100);
+
+  fill(255);
+  rect(200, 250, 75, 50);
+
+  fill("orange");
   textSize(32);
-  text("happy copenhagen", 50, 80);
+  text("hello friends", 50, 80);
 }
